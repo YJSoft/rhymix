93c6e8996fcdd5bcb8a33e8647af80197d86c71c
Kijin Sung <kijin@kijinsung.com>
2020-12-11 20:54:56 +0900

    Add color_scheme_light/color_scheme_dark classes to body

--- classes/context/Context.class.php
+++ classes/context/Context.class.php
@@ -2434,9 +2434,13 @@ class Context
 	 */
 	public static function getBodyClass()
 	{
-		self::$_instance->body_class = array_unique(self::$_instance->body_class);
+		$class_list = self::$_instance->body_class;
+		if (($color_scheme = self::getColorScheme()) !== 'none')
+		{
+			$class_list[] = 'color_scheme_' . $color_scheme;
+		}
 
-		return (count(self::$_instance->body_class) > 0) ? sprintf(' class="%s"', join(' ', self::$_instance->body_class)) : '';
+		return (count($class_list) > 0) ? sprintf(' class="%s"', implode(' ', array_unique($class_list))) : '';
 	}
 
 	/**
